28af4d4e4596e3a6e1d4f6583480f81a7708300f
sannevanseeventer <sanne.van.seeventer@hva.nl>
2023-11-28 20:34:47 +0100

    Partner logo's on scroll

--- src/lib/components/Partners.svelte
+++ src/lib/components/Partners.svelte
@@ -3,79 +3,57 @@
   console.log(data)
 </script>
 
-<section
-  class="sponsor section"
->
-  <div
-    class="sponsor__container container grid"
-  >
-    {#each data.partner as partner}
+<!-- <section class="sponsor section">
+  <div class="sponsor__container container grid"> -->
+
+    <div class="scroll-horizontal">      
+      {#each data.partner as partner}
       <a href={partner.link}>
         <img src={partner.image.url} alt="logo's partners" class="sponsor-img"/></a>
     {/each}
   </div>
-</section>
+
+  <!-- </div>
+</section> -->
 
 <style>
-  .sponsor {
-    position: relative;
-    padding: 6rem
-      3.5rem;
-  }
-
-  .sponsor__container {
-    display: grid;
-    grid-template-columns: repeat(
-      auto-fit,
-      minmax(
-        110px,
-        1fr
-      )
-    );
-    justify-items: center;
-    row-gap: 3.5rem;
+  .scroll-horizontal {
+    overflow-x: auto;
+    display:flex;
     align-items: center;
-  }
-
-  .sponsor__img {
-    filter: invert(
-      0.5
-    );
-  }
+    flex-direction: row;
+    flex-wrap: nowrap;
+    gap: 2rem;
+    scroll-snap-type: x mandatory;
+    padding:1rem 1rem;
+}
 
   .sponsor-img {
-    width: 90px;
+    width: 100px;
     height: auto;
     opacity: 0.5;
-    filter: invert(
-      0.7
-    );
+    filter: invert(0.5);
     transition: 0.3s;
   }
 
   /* Responsive */
   @media screen and (min-width: 720px) {
-    .sponsor__container {
-      grid-template-columns: repeat(
-        3,
-        1fr
-      );
-      justify-items: center;
-      row-gap: 3.5rem;
-    }
-
     .sponsor-img {
-      width: 80px;
+      width: 100px;
       opacity: 0.5;
-      filter: invert(
-        0.7
-      );
+      filter: invert(0.7);
       transition: 0.3s;
     }
   }
   @media screen and (min-width: 768px) {
     .sponsor-img {
-      width: 90px;
+      width: 150px;
     }
+      
+  .scroll-horizontal {
+    gap: 8rem;
+    padding:5rem 5rem;
+}
   }
+
 </style>
